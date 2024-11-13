9fd57d3fd8a9ede0ea712c802b47d0972ae1b40e
tsorelle <terry.sorelle@outlook.com>
2024-11-13 06:35:26 -0600

    phone lists and landing pages for mailings

---
+++ web.root/application/content/pages/focsle.php
@@ -0,0 +1,12 @@
+<style>
+
+</style>
+
+<div class="mb-5">
+    <h2>Virtual Focsle Music</h2>
+    <p>Two Quakers Music is pleased to assist Foscle Music of the UK to present online virtual sing-arounds.</p>
+    <p>Currently these events are precented twice a month. You may <a href="#">click this link to sign up for
+            email invitations here</a></p>
+
+
+</div>
---
+++ web.root/application/content/pages/online.php
@@ -0,0 +1,45 @@
+<h2>Online Singarounds</h2>
+<p>Two Quakers Music is pleased to present three online singarounds (aka song circles)</p>
+<ul>
+    <li>Stone Soup Sessions</li>
+    <li>Celtic Sessions</li>
+    <li>Virtual Focsle Music Singaround<br>
+        (presented in collaboration with the <a href="https://www.focsle.org/">
+            Focsle Music</a> of Southampton, Uk)
+    </li>
+</ul>
+
+<h2>Find out more</h2>
+<div class="row mb-3">
+    <div class="col-md-6 p-2">
+        <h4>About online sessions</h4>
+        <ul>
+            <li>
+                <a href="https://home.2quakers.net/music/tune-and-song-circle">Sign up here for Stone Soup and Celtic Sessions</a>
+            </li>
+            <li>
+                <a href="https://home.2quakers.net/music/focsle-virtual">Sign up here for Focsle Virtual Music</a>
+            </li>
+            <li>
+                <a href="https://www.focsle.org/#online">Learn more about Virtual Focsle Music</a>
+            </li>
+
+        </ul>
+    </div>
+    <div class="col-md-6 p-2">
+        <h4>About zoom video conferencing</h4>
+        <ul>
+            <li><a href="/zoomphones">Phone access </li>
+            <li><a href="https://home.2quakers.net/music/tune-and-song-circle/how-circle-works" target="_blank">How we do an online song circle.</a></li>
+            <li><a href="https://home.2quakers.net/music/tune-and-song-circle/zoom-music" target="_blank">How to optimize Zoom video conferencing for music sharing</a></li>
+            <li><a href="https://home.2quakers.net/music/tune-and-song-circle/keep-picking"  target="_blank">Chip Bach's advice on virtual music making</a></li>
+            <li><a href="https://home.2quakers.net/music/tune-and-song-circle/configure-zoom-original-sound"  target="_blank">Configure Zoom for Original Sound</a></li>
+            <li><a href="https://home.2quakers.net/music/tune-and-song-circle/equipment-zoom-music"  target="_blank">Equipment for Zoom Music</a></li>
+        </ul>
+    </div>
+</div>
+<div class="row mb-0">
+    <div class="col-12 mb-2">
+        <a href="https://home.2quakers.net/music" target="_blank">More about music on home.2quakers.net</a>
+    </div>
+</div>
