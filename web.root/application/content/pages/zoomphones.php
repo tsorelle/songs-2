6f92b4ebf4d2509c626cee00322678319adf7541
tsorelle <terry.sorelle@outlook.com>
2024-11-15 09:38:53 -0600

    phone lists and landing pages for mailings

---
+++ web.root/application/content/pages/zoomphones.php
@@ -0,0 +1,32 @@
+<style>
+
+</style>
+
+<div class="row mb-5">
+
+    <h2>Join our meetings by phone</h2>
+    <div class="row mb-3">
+        <div class="col-md-12 p-2">
+            <h3>One-tap mobile</h3>
+            <p>For Focsle Virtual only</p>
+            <p>On supported devices such as iPhone, one tap mobile does not require meeting id and password</p>
+            <p>
+                <a href="tel:+19292056099,,93966207447#,,,,*580765#">Tap to join  (New York)</a><br><br>
+                <a href="tel:+13126266799,,93966207447#,,,,*580765# US"> Tap to join (Chicago)</a><br>
+            </p>
+        <h3>Dial by your location</h3>
+    <p>+1 929 205 6099 US (New York)<br />+1 312 626 6799 US (Chicago)<br />+1 301 715 8592 US (Washington DC)<br />+1 253 215 8782 US (Tacoma)<br />+1 346 248 7799 US (Houston)<br />+1 669 900 6833 US (San Jose)<br />+44 131 460 1196 United Kingdom<br />+44 203 481 5237 United Kingdom<br />+44 203 481 5240 United Kingdom<br />+44 203 901 7895 United Kingdom<br />+44 208 080 6591 United Kingdom<br />+44 208 080 6592 United Kingdom<br />+44 330 088 5830 United Kingdom<br />+353 1 653 3897 Ireland<br />+353 1 653 3898 Ireland<br />+353 6 163 9031 Ireland<br />+353 1 240 8941 Ireland<br />+353 1 536 9320 Ireland<br />+353 1 653 3895 Ireland</p>
+
+
+        <p>Find your local number: <a href="https://zoom.us/u/abYLvXOnUB" target="_blank">https://zoom.us/u/abYLvXOnUB</a></p>
+            <p>To join for audio only by dial-in phone<b>:</b></p>
+
+            <ol>
+                <li>Dail a number from the list</li>
+                <li>When you hear the instruction, type the Meeting ID (numbers only) followed by the pound sign (#)</li>
+                <li>No passcode (or password) is required for dial-in participants. If you are prompted for a participant id, just press the pound sign (#) again.</li>
+            </ol>
+
+        </div>
+    </div>
+</div>
